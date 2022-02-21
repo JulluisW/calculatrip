dc50633c03c9303d51f440294af0fff9257aefaa
Rodhey48 <rinter18@gmail.com>
2022-02-21 09:50:31 +0700

    upload image kit

--- server/middlewares/uploadToImagekit.js
+++ server/middlewares/uploadToImagekit.js
@@ -2,50 +2,50 @@ const FormData = require('form-data')
 const instanceAxios = require('../apis/axios')
 
 const uploadToImagekit = async (req, res, next) => {
-  try {
-      if (req.file.mimetype !== 'image/png') {
-          if (req.file.mimetype !== 'image/jpeg') {
-              throw {
-                  name: 'InvalidImageFormat'
-              }
-          }
-      }
-      if (req.file.size > 3000000) {
-          throw {
-              name: 'InvalidImageSize'
-          }
-      }
+    try {
+        if (req.file.mimetype !== 'image/png') {
+            if (req.file.mimetype !== 'image/jpeg') {
+                throw {
+                    name: 'InvalidImageFormat'
+                }
+            }
+        }
+        if (req.file.size > 3000000) {
+            throw {
+                name: 'InvalidImageSize'
+            }
+        }
 
-      if (req.file.buffer) {
-          const data = req.file.buffer.toString("base64")
-          const dataName = req.file.originalname
-          let form = new FormData()
-          form.append('file', data)
-          form.append('fileName', dataName)
+        if (req.file.buffer) {
+            const data = req.file.buffer.toString("base64")
+            const dataName = req.file.originalname
+            let form = new FormData()
+            form.append('file', data)
+            form.append('fileName', dataName)
 
-          let newHeaders = form.getHeaders()
+            let newHeaders = form.getHeaders()
 
-          const response = await instanceAxios.post("/files/upload", form, {
-              headers: newHeaders
-          })
-          if (!response) {
-              throw {
-                  name: 'Error Create Data'
-              }
-          }
+            const response = await instanceAxios.post("/files/upload", form, {
+                headers: newHeaders
+            })
+            if (!response) {
+                throw {
+                    name: 'Error Create Data'
+                }
+            }
 
-          req.uploadUrl = response.data.url
+            req.uploadUrl = response.data.url
 
-          next()
-      } else {
-          throw {
-              name: 'Cant read file image file'
-          }
-      }
+            next()
+        } else {
+            throw {
+                name: 'Cant read file image file'
+            }
+        }
 
-  } catch (err) {
-      next(err)
-  }
+    } catch (err) {
+        next(err)
+    }
 
 
 }
